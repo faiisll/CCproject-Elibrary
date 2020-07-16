a9d31451deaa4a883aa772a9701a8bdd43af1606
faiisll <faisal.ayas72@gmail.com>
2020-07-16 13:23:23 +0700

    update book

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -10,18 +10,18 @@ const getBook = () => {
             
             html += `
             <tr>
-                <td>${count+1}</td>
-                <td>${doc.data().title}</td>
-                <td>${doc.data().description.slice(0, 100)}</td>
-                <td>${doc.data().category}</td>
-                <td>${doc.data().img}</td>
-                <td>${doc.data().pdf}</td>
-                <td>
-                <button class="btn btn-sm btn-primary" onclick="editBook(${doc.data().id})">Edit</button>
-                </td>
+            <td>${count+1}</td>
+            <td>${doc.data().title}</td>
+            <td>${doc.data().description.slice(0, 100)}</td>
+            <td>${doc.data().category}</td>
+            <td>${doc.data().img}</td>
+            <td>${doc.data().pdf}</td>
+            <td>
+            <button class="btn btn-sm btn-primary" onclick="editBook(${doc.id})">Edit</button>
+            </td>
             </tr>
             `;
-
+            
             count ++;
         });
         bookscon.innerHTML = html;
@@ -33,7 +33,7 @@ const getBook = () => {
 const formAdd = document.querySelector('#form-add');
 formAdd.addEventListener('submit', (e) => {
     e.preventDefault();
-
+    
     db.collection("books").doc(`${count}`).set({
         title: formAdd['title'].value,
         description: formAdd['desc'].value,
@@ -54,3 +54,52 @@ formAdd.addEventListener('submit', (e) => {
 db.collection('books').onSnapshot( snapshot => {
     getBook();
 });
+
+const formEdit = document.querySelector('#form-edit');
+formEdit.addEventListener('reset', () => changeForm(0));
+formEdit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection("books").doc(`${formEdit['id'].value}`).update({
+        title: formEdit['title'].value,
+        description: formEdit['desc'].value,
+        category: formEdit['category'].value,
+        img: formEdit['img'].value,
+        pdf: formEdit['pdf'].value
+    }).then((d) => {
+        alert('Success Update data!');
+        formEdit.reset();
+        changeForm(0);
+    });
+});
+
+const editBook = id => {
+    changeForm(1);
+    db.collection("books").doc(`${id}`)
+    .get()
+    .then(function(doc) {
+        if (doc.exists) {
+            formEdit['id'].value = doc.id;
+            formEdit['title'].value = doc.data().title;
+            formEdit['desc'].value = doc.data().description;
+            formEdit['category'].value = doc.data().category;
+            formEdit['img'].value = doc.data().img;
+            formEdit['pdf'].value = doc.data().pdf;
+            
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+};
+
+const changeForm = stat => {
+    if(stat == 1){
+        formAdd.style.display = "none";
+        formEdit.style.display = "block";
+    }else{
+        formAdd.style.display = "block";
+        formEdit.style.display = "none";
+    }
+}
